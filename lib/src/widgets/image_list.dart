45f91568b463011232fe3d2dd58e577068163697
Ansh Gujral <anshgujral1997@gmail.com>
2018-12-23 11:52:01 +0530

    added title with each image
    Signed-off-by: Ansh Gujral <anshgujral1997@gmail.com>

--- lib/src/widgets/image_list.dart
+++ lib/src/widgets/image_list.dart
@@ -7,15 +7,24 @@ class ImageList extends StatelessWidget {
     return ListView.builder(
       itemCount: images.length,
       itemBuilder: (context, int index) {
-        return Container(
+        return buildImage(images[index]);
+      },
+    );
+  }
+
+  Widget buildImage(ImageModel image) {
+    return Container(
           decoration: BoxDecoration(
             border: Border.all(color: Colors.grey)
           ),
           padding: EdgeInsets.all(10.0),
           margin: EdgeInsets.all(20.0),
-          child: Image.network(images[index].url),
+          child: Column(
+            children: <Widget>[
+              Image.network(image.url),
+              Text(image.title)
+            ],
+          ),
         );
-      },
-    );
   }
 }
